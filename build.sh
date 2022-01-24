c342bbf91c38741415010090a6404811d09e3438
OwenFeik <owenhfeik@gmail.com>
2022-01-24 13:36:53 +1100

    clean up script to use 'set -e', rebuild database on schema change

--- build.sh
+++ build.sh
@@ -1,22 +1,38 @@
+set -e
+
 dir=$(realpath $(dirname "$0"))
 
 if [ "$1" == "hard" ]; then
     rm -rf "$dir/target"
 fi
 
-cd "$dir/client" \
-    && wasm-pack build --target web \
-    && cd "$dir" \
-    && cargo build -p server \
-    && data="$dir/target/debug/data" \
-    && mkdir -p "$data" \
-    && content="$data/content" \
-    && mkdir -p "$content" \
-    && ln -sf "$dir/client/pkg" "$content/pkg" \
-    && cp "$dir/client/index.html" "$content/" \
-    && sqlite3 "$data/database.db" < "$dir/server/schema.sql" \
-    && echo "serving $content at http://localhost:3030/static/" \
-    && \
-        RUST_BACKTRACE=1 \
-        DATABASE_URL="$data/database.db" \
-        "$dir/target/debug/server" "$content"
+# Build client
+cd "$dir/client"
+wasm-pack build --target web
+
+# Build server
+cd "$dir"
+cargo build -p server
+
+data="$dir/target/debug/data"
+mkdir -p "$data"
+
+# Create database if schema changed / not run
+schema="$data/schema.sql"
+if [ ! -f "$schema" ] || cmp --silent "$dir/server/schema.sql" "$schema"; then
+    rm -f "$data/database.db"
+    cp "$dir/server/schema.sql" "$data/"
+    sqlite3 "$data/database.db" < "$data/schema.sql"
+fi
+
+# Copy across content
+content="$data/content"
+mkdir -p "$content"
+ln -sf "$dir/client/pkg" "$content/pkg"
+cp "$dir/client/index.html" "$content/"
+
+# Start server
+echo "serving $content at http://localhost:3030/static/"
+RUST_BACKTRACE=1 \
+    DATABASE_URL="$data/database.db" \
+    "$dir/target/debug/server" "$content"
